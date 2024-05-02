f37292d26fccdea088effdd3adf2e19ec4c803c4
Arhan <ali.arhan19926@gmail.com>
2024-05-02 22:20:41 +0530

    design change

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,14 @@
+
 import GoogleLoginButton from "./googleLoginButton";
 import "./css/login.css";
 const Login = () => {
   return (
     <div className="mainContainer">
       <div className="leftContainer">
-        <h2>Welcome!</h2>
-        <p>Please Login to access KOTHMIR</p>
+        <div className="logoContainer">
+          <img className="loginPageImage" src="/kothmir_login.png"></img>
+          <h2>Kothmir</h2>
+        </div>
         <GoogleLoginButton />
       </div>
       <div className="rightContainer"></div>
