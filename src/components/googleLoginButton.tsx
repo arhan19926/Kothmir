f1360d42462ae94ee7a9470210c1895c1d052fee
Arhan <ali.arhan19926@gmail.com>
2024-05-08 12:37:26 +0530

    added outlet to routes for persistent layout in all pages

--- src/components/googleLoginButton.tsx
+++ src/components/googleLoginButton.tsx
@@ -1,18 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { STATUS_CODES } from "../utils/constants/status-codes.constant";
 import { BACKEND_ENDPOINT } from "../utils/constants/common.constant";
+import { useState } from "react";
 
 const GoogleLoginButton = () => {
-  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleGoogleLogin = async () => {
+    setIsLoading(true);
     try {
       const apiData = await fetch(BACKEND_ENDPOINT);
       const result = await apiData.json();
 
-      if (result.statusCode === STATUS_CODES.OK) {
+      if (result.statusCode === STATUS_CODES.OK && result.data) {
+        // window.open(result.data, "_blank");
         window.location.href = result.data;
       } else {
-        navigate("/");
+        console.error("Invalid response from Backend");
       }
     } catch (error) {
       console.log(`error--->`, error);
@@ -21,8 +25,11 @@ const GoogleLoginButton = () => {
   };
 
   return (
-    <button className="loginButton" onClick={() => handleGoogleLogin()}>
-      Login with Google
+    <button
+      className="loginButton"
+      onClick={() => !isLoading && handleGoogleLogin()}
+    >
+      {isLoading ? "Loading..." : "Login with Google"}
     </button>
   );
 };
--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
 import "./layout.css";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Layout = ({ children }: { children: any }) => {
+const Layout = () => {
   return (
     <div className="wrapper">
       <Sidebar />
-      <div className="childWrapper"> {children}</div>
+      <div className="childWrapper">
+        <Outlet />
+      </div>
     </div>
   );
 };
