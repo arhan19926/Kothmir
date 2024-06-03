92956d990d8fa5159e4573db3890dff815219356
Arhan <ali.arhan19926@gmail.com>
2024-06-03 11:07:20 +0530

    Added blogs and about pages routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,8 @@ import Login from "../components/Login";
 import Home from "../pages/home/home";
 import Layout from "../pages/layout/layout";
 import Recipes from "../pages/recipes/recipes";
+import About from "../pages/about/about";
+import Blogs from "../pages/blogs/blogs";
 
 // const layoutRoutes = (Component: JSX.Element) => <Layout>{Component}</Layout>;
 
@@ -22,6 +24,14 @@ const routes = createBrowserRouter([
       {
         path: "/recipes",
         element: <Recipes />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
       },
     ],
   },
